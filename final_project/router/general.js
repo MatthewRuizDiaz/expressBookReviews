25ead1a8fdda2da10b78c6eeb5106da8c8eda9e7
Matthew Ruiz Diaz <matthew.ruizdiaz@gmail.com>
2025-03-09 20:06:09 -0400

    Changed implementation of general user endpoints to be asynchronous

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const axios = require('axios');
 
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
@@ -24,43 +24,50 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  res.send(JSON.stringify(books,null,4));
-});
+public_users.get('/', async function (req, res) {
+    try {
+      const response = await axios.get('https://matthewruizd-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/');
+      res.send(JSON.stringify(response.data, null, 4));
+    } catch (error) {
+      res.status(500).send('Error retrieving books');
+    }
+  });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
-});
+public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn;
+    try {
+      const response = await axios.get(`https://matthewruizd-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/isbn/${isbn}`);
+      res.send(response.data);
+    } catch (error) {
+      res.status(500).send('Error retrieving book details');
+    }
+  });
   
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author;
-    let booksByAuthor = [];
-    for (let key in books) {
-      if (books[key].author === author) {
-        booksByAuthor.push(books[key]);
-      }
+    try {
+      const response = await axios.get(`https://matthewruizd-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/author/${author}`);
+      const booksByAuthor = response.data.filter(book => book.author === author);
+      res.send(booksByAuthor);
+    } catch (error) {
+      res.status(500).send('Error retrieving books by author');
     }
-    res.send(booksByAuthor);
   });
   
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  const title = req.params.title;
-  let book = [];
-  for (let key in books) {
-    if(books[key].title === title) {
-        book.push(books[key]);
+public_users.get('/title/:title', async function (req, res) {
+    const title = req.params.title;
+    try {
+      const response = await axios.get(`https://matthewruizd-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/title/${title}`);
+      const booksByTitle = response.data.filter(book => book.title === title);
+      res.send(booksByTitle);
+    } catch (error) {
+      res.status(500).send('Error retrieving books by title');
     }
-  }
-  res.send(book);
-});
+  });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
